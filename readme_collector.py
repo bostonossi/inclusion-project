5fe7a7c34db7c98fa0348276e708ec3685b090e7
Yongyuan Huang <yongyuanhuang@dhcp-wifi-8021x-155-41-44-169.bu.edu>
2019-08-02 11:59:05 -0400

    readme state collector

---
+++ readme_collector.py
@@ -0,0 +1,59 @@
+from coc_collector import *
+
+class Readme_Statuses(Enum):
+    """
+    This class enumerates the various potential states of a repository's
+    code of conduct.
+    """
+    GIT_DET_NONE = 1
+    GIT_DET_YES = 2
+
+class Readme_Collector(OSSDI_Collector):
+    """
+    This class collects data on the state of a given repository's Readme
+    file, and populate the corresponding table in the Bossdi database.
+    """
+    def __init__(self, api_key):
+        super().__init__(api_key)
+
+        exists = self.session.query(self.session.query(Readme_State).filter_by\
+                                  (state='GitHub_det_Yes').exists()).scalar()
+        if not exists:
+            self.session.add(Readme_State(state = 'GitHub_det_Yes'))
+            self.session.add(Readme_State(state = 'GitHub_det_No'))
+            self.session.commit()
+
+    def readme(self, owner, repo):
+        """
+        Determines whether a given repository has a Readme file
+        and record the result in the Bossdi database.
+
+        :param owner: The name of the Project owner.
+        :param repo: The name of the repo
+        :return: DataFrame with the result
+        """
+        
+        url = "http://api.github.com/repos/{}/{}/readme".format\
+              (owner, repo)
+        json = requests.get(url, auth=('user', self.GITHUB_API_KEY)).json()
+
+        exists = self.session.query(self.session.query(Projects).filter_by\
+                                  (repo_name = repo).exists()).scalar()
+
+        if json['content'] != None:
+            if not exists:
+                self.session.add(Projects(repo_owner = owner, repo_name = repo, readme_state = 2))
+                self.session.commit()
+            else:
+                repo_object = self.session.query(Projects).filter_by(repo_name = repo).first()
+                repo_object.readme_state = 2
+                self.session.commit()   
+        else:
+            if not exists:
+                self.session.add(Projects(repo_owner = owner, repo_name = repo, readme_state = 1))
+                self.session.commit()
+            else:
+                repo_object = self.session.query(Projects).filter_by(repo_name = repo).first()
+                repo_object.readme_state = 1
+                self.session.commit()
+
